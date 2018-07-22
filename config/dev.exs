9011d89c4e7ba9ec3664435c92119f50400b4c2d
Ettore Berardi <ettore.berardi@bbc.co.uk>
2018-07-22 16:12:12 +0100

    Adds per-environment configs.

---
+++ config/dev.exs
@@ -0,0 +1,3 @@
+use Mix.Config
+
+config :fetcher, :environment, :dev
--- lib/mozart_fetcher/local_cache.ex
+++ lib/mozart_fetcher/local_cache.ex
@@ -1,8 +1,8 @@
 defmodule MozartFetcher.LocalCache do
   def get_or_store(id, f) do
-    case Mix.env do
-      :test -> f.()
-      _ -> ConCache.get_or_store(:fetcher_cache, id, f)
+    case Application.get_env(:my_app, :environment) do
+      :prod -> ConCache.get_or_store(:fetcher_cache, id, f)
+      _ -> f.()
     end
   end
 end
