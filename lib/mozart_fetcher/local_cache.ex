213caca4cbfe1e21f421d2e35b9f0009b016aa2c
Ettore Berardi <ettore.berardi@bbc.co.uk>
2018-07-22 16:16:12 +0100

    Simpler solution for environment config.

--- lib/mozart_fetcher/local_cache.ex
+++ lib/mozart_fetcher/local_cache.ex
@@ -1,6 +1,6 @@
 defmodule MozartFetcher.LocalCache do
   def get_or_store(id, f) do
-    case Application.get_env(:my_app, :environment) do
+    case Application.get_env(:fetcher, :environment) do
       :prod -> ConCache.get_or_store(:fetcher_cache, id, f)
       _ -> f.()
     end
