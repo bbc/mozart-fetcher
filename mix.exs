976b7fed1b60ae0a0748556d6f9939335aa5a771
Sam French <Sam.French@bbc.co.uk>
2018-11-09 15:16:59 +0000

    Update dependencies

--- mix.exs
+++ mix.exs
@@ -27,7 +27,7 @@ defmodule MozartFetcher.MixProject do
       {:poison, "~> 3.1"},
       {:httpoison, "~> 1.0"},
       {:con_cache, "~> 0.13.0"},
-      {:distillery, "~> 1.5", runtime: false},
+      {:distillery, "~> 2.0", runtime: false},
       {:mock, "~> 0.3.0", only: :test},
       {:parse_trans, "~> 3.2.0"}
 
