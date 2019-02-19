7551a28de879a09f79c10349d94f2e143b93e4ed
Ryan Wood <Ryan.Wood01@bbc.co.uk>
2019-02-19 14:16:48 +0000

    Use very short timeout for unit tests

--- lib/mozart_fetcher/http_client.ex
+++ lib/mozart_fetcher/http_client.ex
@@ -1,8 +1,10 @@
 defmodule HTTPClient do
   def get(endpoint) do
     cert = Application.get_env(:mozart_fetcher, :dev_cert_pem)
+    timeout = Application.get_env(:mozart_fetcher, :timeout)
+
     headers = []
-    options = [recv_timeout: 3000, ssl: [certfile: cert]]
+    options = [recv_timeout: timeout, ssl: [certfile: cert]]
 
     HTTPoison.get(endpoint, headers, options)
   end
