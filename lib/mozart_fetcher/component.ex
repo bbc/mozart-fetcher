ab8c41fa925fa7b1cc646c3c6b181cf43722ab8b
James Bowers <james.bowers@bbc.co.uk>
2019-04-05 14:29:05 +0100

    more intuitive logging messages and metric increments

--- lib/mozart_fetcher/component.ex
+++ lib/mozart_fetcher/component.ex
@@ -8,10 +8,17 @@ defmodule MozartFetcher.Component do
     process(config, get(config))
   end
 
-  defp process(config, {:ok, %HTTPoison.Response{status_code: status_code, body: body}}) do
+  defp process(config, {:ok, %HTTPoison.Response{status_code: 200, body: body}}) do
     ExMetrics.increment("success.component.process")
-    ExMetrics.increment("success.component.process.#{config.id}.#{status_code}")
-    %Component{index: 0, id: config.id, status: status_code, envelope: Envelope.build(body)}
+    ExMetrics.increment("success.component.process.#{config.id}.200")
+    %Component{index: 0, id: config.id, status: 200, envelope: Envelope.build(body)}
+  end
+
+  defp process(config, {:ok, %HTTPoison.Response{status_code: status_code, body: body}}) do
+    ExMetrics.increment("error.component.process")
+    ExMetrics.increment("error.component.process.#{config.id}.#{status_code}")
+    Stump.log(:error, %{message: "Non-200 response. Got status:#{status_code} for component #{config.id}"})
+    %Component{index: 0, id: config.id, status: status_code, envelope: %Envelope{}}
   end
 
   defp process(config, {:error, %HTTPoison.Error{reason: reason}}) do
