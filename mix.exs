26fe26491aaeb519e5a5307c111d123a247cb761
Ettore Berardi <ettore.berardi@bbc.co.uk>
2019-05-14 16:12:00 +0100

    Updates Fetcher version.

--- mix.exs
+++ mix.exs
@@ -4,8 +4,8 @@ defmodule MozartFetcher.MixProject do
   def project do
     [
       app: :mozart_fetcher,
-      version: "0.1.0",
-      elixir: "~> 1.6",
+      version: "1.0.0",
+      elixir: "~> 1.7",
       start_permanent: Mix.env() == :prod,
       deps: deps()
     ]
