dcf59b56e09194187874fc474dba282a9ccdadf1
Ettore Berardi <ettore.berardi@bbc.co.uk>
2019-05-20 11:27:28 +0100

    A more verbose approach to get and store cache on ETS.

--- test/local_cache_test.exs
+++ test/local_cache_test.exs
@@ -8,18 +8,33 @@ defmodule MozartFetcher.LocalCacheTest do
     :ok
   end
 
-  describe "a successful response" do
+  describe "a successful 200 response" do
     test "store the results in cache" do
-      f = fn () -> {:ok, "valid response"} end
-      LocalCache.get_or_store("abc", f)
-      assert cache() == [{"abc", {:ok, "valid response"}}]
+      response = {:ok, %HTTPoison.Response{status_code: 200, body: "valid response!"}}
+
+      f = fn () -> response end
+
+      assert LocalCache.get_or_store("abc", f) == response
+      assert cache() == [{"abc", response}]
+    end
+  end
+
+  describe "a successful 404 response" do
+    test "will NOT store the results in cache" do
+      response = {:ok, %HTTPoison.Response{status_code: 404, body: "not found :()"}}
+      f = fn () -> response end
+
+      assert LocalCache.get_or_store("abc", f) == response
+      assert cache() == []
     end
   end
 
   describe "an error response" do
     test "store the results in cache" do
-      f = fn () -> {:error, "NOT valid response"} end
-      LocalCache.get_or_store("abc", f)
+      response = {:error, %HTTPoison.Error{id: nil, reason: :econnrefused}}
+
+      f = fn () -> response end
+      assert LocalCache.get_or_store("abc", f) == response
       assert cache() == []
     end
   end
