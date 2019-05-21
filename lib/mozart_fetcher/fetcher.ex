02bb08d4d1a0f4a5b3670ae166e8904f272a7a17
Sam French <Sam.French@bbc.co.uk>
2019-05-21 15:28:39 +0100

    Add component max timeout to task await

--- lib/mozart_fetcher/fetcher.ex
+++ lib/mozart_fetcher/fetcher.ex
@@ -1,5 +1,5 @@
 defmodule MozartFetcher.Fetcher do
-  alias MozartFetcher.{Component}
+  alias MozartFetcher.{Component, TimeoutParser}
 
   use ExMetrics
 
@@ -12,12 +12,19 @@ defmodule MozartFetcher.Fetcher do
   def process(components) do
     ExMetrics.timeframe "function.timing.fetcher.process" do
       components
-      |> Enum.with_index
+      |> Enum.with_index()
       |> Enum.map(&Task.async(fn -> Component.fetch(&1) end))
-      |> Enum.map(&Task.await/1)
+      |> Enum.map(&Task.await(&1, max_timeout(components)))
       |> decorate_response
       |> Jason.encode!()
     end
+  end
+
+  defp max_timeout(components) do
+    components
+    |> Enum.map(&Map.get(&1, :endpoint))
+    |> Enum.map(&TimeoutParser.parse/1)
+    |> Enum.max()
   end
 
   defp decorate_response(envelopes) do
