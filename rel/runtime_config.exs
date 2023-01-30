df37eed940b856cfa7c61cb60ebffdba39b00ee3
Joseph Pack <josephapack@hotmail.co.uk>
2023-01-30 16:59:21 +0000

    Remove setting of PRODUCTION_ENV in runtime conf

--- rel/runtime_config.exs
+++ rel/runtime_config.exs
@@ -1,15 +1,4 @@
 use Mix.Config
 
-[
-  "PRODUCTION_ENVIRONMENT"
-]
-|> Enum.each(fn config_key ->
-  if System.get_env(config_key) == nil do
-    raise "Config not set in environment: #{config_key}"
-  end
-  config :mozart_fetcher,
-         Keyword.new([{String.to_atom(String.downcase(config_key)), System.get_env(config_key)}])
-end)
-
 config :statix,
   tags: ["BBCEnvironment:#{System.get_env("PRODUCTION_ENVIRONMENT")}"]
