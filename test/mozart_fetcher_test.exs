fc1807bdce3906b2f4e25111d436d726356f1562
Andreas Ward <andreas.ward@bbc.co.uk>
2026-03-17 09:44:47 +0000

    Added ssl_override: true to try and resolve previous certificate/tls errors

--- test/mozart_fetcher_test.exs
+++ test/mozart_fetcher_test.exs
@@ -6,7 +6,7 @@ defmodule MozartFetcherTest do
 
   test "request_ssl/0 on non-prod environment" do
     certfile = System.get_env("DEV_CERT_PEM")
-    assert [certfile: certfile, verify: :verify_none] == MozartFetcher.request_ssl()
+    assert [certfile: certfile, verify: :verify_none, ssl_override: true] == MozartFetcher.request_ssl()
   end
 
   describe "on prod environment" do
@@ -26,7 +26,8 @@ defmodule MozartFetcherTest do
       assert [
                certfile: @fake_cert_file_path,
                keyfile: @fake_key_file_path,
-               verify: :verify_none
+               verify: :verify_none,
+                ssl_override: true
              ] ==
                MozartFetcher.request_ssl()
     end
