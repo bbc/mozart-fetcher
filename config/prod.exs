682cdf0e80e3d35ada5cc3f8218964579f559547
Aleksandar <isavitaisa@gmail.com>
2026-03-24 08:39:39 +0000

    Fix 'warning: the :backends key for the :logger application' (#103)
    * Fix 'warning: the :backends key for the :logger application'
    
    * Disable the default logger to avoid consol log

--- config/prod.exs
+++ config/prod.exs
@@ -1,10 +1,9 @@
 import Config
 
-config :logger,
-  backends: [{LoggerFileBackend, :file}]
-
 config :logger, :file,
   path: "/var/log/component/app.log",
   format: {MozartFetcher.Logger.Formatter, :format},
   metadata: :all,
   level: :error
+
+config :logger, :default_handler, false
